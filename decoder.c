727b1f99b2f5aeb1c7ad0828f42d01b8f5574547
Vytautas Šaltenis <vytas@rtfb.lt>
2025-12-14 20:55:01 +0200

    Partially generalize decoder

--- decoder.c
+++ decoder.c
@@ -38,7 +38,7 @@ uint32_t lut[16] = {
     0x01010101u,
 };
 
-void decoder_test(uint8_t input, uint8_t *counters, size_t ncounters) {
+void decode_one_8b(uint8_t input, uint8_t *counters, size_t ncounters) {
     uint32_t lower_nibble = input & 0xf;
     uint32_t upper_nibble = (input >> 4) & 0xf;
     uint32_t *counter32 = (uint32_t *)counters;
@@ -47,18 +47,38 @@ void decoder_test(uint8_t input, uint8_t *counters, size_t ncounters) {
     *counter32 += lut[upper_nibble];
 }
 
-void decode_output_8b(uint8_t *output_layer, uint8_t *classes_out) {
-    int max_sum[8] = {0};
-    for (int class = 0; class < NUM_CLASSES; class++) {
-        uint8_t counters[8] = {0};
-        for (int i = 0; i < BITS_PER_CLASS; i++) {
-            decoder_test(output_layer[class*BITS_PER_CLASS+i], counters, 8);
-        }
-        for (int i = 0; i < 8; i++) {
-            if (counters[i] > max_sum[i]) {
-                max_sum[i] = counters[i];
-                classes_out[i] = class;
-            }
-        }
-    }
+void decode_one_16b(uint16_t input, uint8_t *counters, size_t ncounters) {
+    uint32_t nibble1 = input & 0xf;
+    uint32_t nibble2 = (input >> 4) & 0xf;
+    uint32_t nibble3 = (input >> 8) & 0xf;
+    uint32_t nibble4 = (input >> 12) & 0xf;
+    uint32_t *counter32 = (uint32_t *)counters;
+    *counter32 += lut[nibble1];
+    counter32++;
+    *counter32 += lut[nibble2];
+    counter32++;
+    *counter32 += lut[nibble3];
+    counter32++;
+    *counter32 += lut[nibble4];
 }
+
+#define DECODE(suffix, elem_type, n)                                          \
+void decode_output_##suffix(elem_type *output_layer, uint8_t *classes_out) {  \
+    int max_sum[n] = {0};                                                     \
+    for (int class = 0; class < NUM_CLASSES; class++) {                       \
+        uint8_t counters[n] = {0};                                            \
+        for (int i = 0; i < BITS_PER_CLASS; i++) {                            \
+            decode_one_##suffix(output_layer[class*BITS_PER_CLASS+i],         \
+                                counters, n);                                 \
+        }                                                                     \
+        for (int i = 0; i < n; i++) {                                         \
+            if (counters[i] > max_sum[i]) {                                   \
+                max_sum[i] = counters[i];                                     \
+                classes_out[i] = class;                                       \
+            }                                                                 \
+        }                                                                     \
+    }                                                                         \
+}
+
+DECODE(8b, uint8_t, 8)
+DECODE(16b, uint16_t, 16)
