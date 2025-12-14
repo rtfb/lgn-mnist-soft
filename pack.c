a9fe5743f77903afe8ea8daef550b00895b73eb7
Vytautas Šaltenis <vytas@rtfb.lt>
2025-12-14 23:09:16 +0200

    Fix 64-bit version
    The bug was in packing. Gnarly. Turns out it was interpreting 0x1 as
    a signed 32-bit constant and thus emitting a 32-bit shl and
    sign-extending it to 64 bits, which broke the upper 32 bits of my data
    in horrible ways.
    
    Fixed by passing a properly typed constant for the 64-bit version.

--- pack.c
+++ pack.c
@@ -2,7 +2,7 @@
 
 #include "consts.h"
 
-#define PACK(suffix, elem_type, num_inputs)                                 \
+#define PACK(suffix, one, elem_type, num_inputs)                            \
 void pack##suffix(uint8_t inp[NUM_INPUTS][256], int out_width,              \
                   elem_type out[num_inputs][256]) {                         \
     int input_base = 0;                                                     \
@@ -10,7 +10,7 @@ void pack##suffix(uint8_t inp[NUM_INPUTS][256], int out_width,              \
         for (int image = 0; image < out_width; image++) {                   \
             for (int i = 0; i < 256; i++) {                                 \
                 elem_type val = out[out_img][i];                            \
-                elem_type bit = (inp[input_base+image][i] & 0x1) << image;  \
+                elem_type bit = (inp[input_base+image][i] & one) << image;  \
                 val |= bit;                                                 \
                 out[out_img][i] = val;                                      \
             }                                                               \
@@ -19,7 +19,7 @@ void pack##suffix(uint8_t inp[NUM_INPUTS][256], int out_width,              \
     }                                                                       \
 }
 
-PACK(8, uint8_t, NUM_8B_INPUTS)
-PACK(16, uint16_t, NUM_16B_INPUTS)
-PACK(32, uint32_t, NUM_32B_INPUTS)
-PACK(64, uint64_t, NUM_64B_INPUTS)
+PACK(8, 0x1, uint8_t, NUM_8B_INPUTS)
+PACK(16, 0x1, uint16_t, NUM_16B_INPUTS)
+PACK(32, 0x1, uint32_t, NUM_32B_INPUTS)
+PACK(64, 0x1ULL, uint64_t, NUM_64B_INPUTS)
